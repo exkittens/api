45f52aaf26dadde4f94bee844ef6fa33c3be95db
relaxcore <yurii.rogulya@teamvoy.com>
2021-03-28 20:03:10 +0300

    Add users schema

---
+++ lib/api/user.ex
@@ -0,0 +1,27 @@
+defmodule Api.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :family_name, :string
+    field :given_name, :string
+    field :locale, :string
+    field :name, :string
+    field :picture_url, :string
+
+    timestamps()
+  end
+
+  @required_fields [:email, :name, :picture_url, :given_name, :family_name, :locale]
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+    |> unique_constraint(:email)
+  end
+end
